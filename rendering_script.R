8130e01b89e189764c9ddeb0abcc7809717dc0ea
AndreaCirilloAC <andreacirilloac@gmail.com>
2017-11-07 21:44:23 +0100

    working example

--- rendering_script.R
+++ rendering_script.R
@@ -2,3 +2,7 @@ n <- 200
 
 data <- data.frame(a = sample(letters,size = n, replace = TRUE),
                    b = seq(1:n))
+
+rmarkdown::render("template.Rmd", params = list(
+  vector = seq(1:100)
+))
